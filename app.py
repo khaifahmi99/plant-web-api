4d41e8521d3515b0596e8cdee3a67d6eac83f507
khaifahmi99 <khaifahmi99@github.com>
2020-09-14 09:25:36 +1000

    complete profile page

--- app.py
+++ app.py
@@ -38,5 +38,9 @@ def search():
         return render_template('search.html', keyword=keyword, data=response)
     return render_template('search.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
