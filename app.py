36d8661a710ef3703e0534782b929ab64215ba5b
khaifahmi99 <khaifahmi99@github.com>
2020-09-13 23:06:54 +1000

    complete hmoe and description page

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, redirect, request
+from flask_cors import CORS
+import json
+import random
+
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+API_KEY = 'brmvB5hePGk4WUVN4aecYf69-9XsWnLgPHjkBjEIgfg'
+
+@app.route('/')
+def home():
+    page = random.randint(1,1000)
+    url = f'https://trefle.io/api/v1/plants?token={API_KEY}&page={page}'
+    response = requests.get(url).text
+    response = json.loads(response)['data']
+
+    return render_template('home.html', data=response)
+
+@app.route('/plant/<id>')
+def plant(id):
+    url = f'https://trefle.io/api/v1/plants/{id}?token={API_KEY}'
+    response = requests.get(url).text
+    response = json.loads(response)['data']
+    return render_template('plant.html', data=response)
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
